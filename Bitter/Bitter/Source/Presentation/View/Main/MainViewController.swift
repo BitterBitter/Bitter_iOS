43758dfb0c0652a14b6f3f87382ea6365dc78f78
DGSWcsh <101883606+DGSWcsh@users.noreply.github.com>
2022-07-19 15:35:35 +0900

    ✨ :: Main UI <- tableView

--- Bitter/Bitter/Source/Presentation/View/Main/MainViewController.swift
+++ Bitter/Bitter/Source/Presentation/View/Main/MainViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 final class MainViewController: baseVC {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    let data = ["1","2"]
+    
     @IBAction func onClickOffbaseCard(_ sender: UIButton) {
         
     }
     
-    @IBOutlet weak var Medicine: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mainTableView.dataSource = self
+        self.mainTableView.delegate = self
+    }
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.mainTableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath)
+        cell.textLabel?.text = self.data[indexPath.row]
+        return cell
+    }
+    
 }
