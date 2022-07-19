a4caed43d14f460af2cba6a6b926016f3bcf66fe
DGSWcsh <101883606+DGSWcsh@users.noreply.github.com>
2022-07-19 15:56:29 +0900

    ✨ :: Login -> Main 화면이동

--- Bitter/Bitter/Source/Presentation/View/Auth/SignIn/SignInViewController.swift
+++ Bitter/Bitter/Source/Presentation/View/Auth/SignIn/SignInViewController.swift
@@ -5,11 +5,18 @@
 //  Created by 김상금 on 2022/07/19.
 //
 
-import Foundation
+import UIKit
 
 final class SignInViewController: baseVC {
     // MARK: - Properties
     @IBOutlet weak var idTextField: TextFieldWithPadding!
     @IBOutlet weak var passwordTextField: TextFieldWithPadding!
+    
+    
+    @IBAction func tapLoginButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "main", bundle: nil)
+        guard let vc = storyboard.instantiateViewController(withIdentifier: "main") as? MainViewController else { return }
+        UIApplication.shared.keyWindow?.replaceRootViewController(vc, animated: true, completion: nil)
+    }
     
 }
